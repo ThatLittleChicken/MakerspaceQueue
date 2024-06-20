9cf27154773c48315eae6ada4252d20e88f7fa1f
Gent Yong <yonggh@byu.edu>
2024-06-19 21:00:21 -0600

    Minor changes

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// The service port defaults to 3000 or is read from the program arguments
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// Text to display for the service name
+const serviceName = process.argv.length > 3 ? process.argv[3] : 'website';
+
+// Serve up the static content
+app.use(express.static('public'));
+
+// Provide the version of the application
+app.get('/config', (_req, res) => {
+  res.send({ version: '1.0.0', name: serviceName });
+});
+
+// Return the homepage if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
