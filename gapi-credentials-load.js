9ac4296a79865348683b58281970689452a25afc
Gent Yong <yonggenthoong@gmail.com>
2024-08-20 15:00:58 -0600

    Fix get gapi refresh token

--- gapi-credentials-load.js
+++ gapi-credentials-load.js
@@ -1,13 +1,10 @@
 const { google } = require("googleapis");
 const fs = require("fs");
 
-//load credentials some how...
+//load credentials
 let creds = null;
-try {
-  creds = require("./gapi-credentials.json");
-} catch (err) {
-  throw `Failed to load credentials.json: ${err}`;
-}
+
+creds = JSON.parse(fs.readFileSync('./box-credentials.json', 'utf8'));
 if (creds.refresh_token === "...") {
   throw `Please run 'node gapi-credentials-fill.js'`;
 }
@@ -19,6 +16,8 @@ const auth = new google.auth.OAuth2(
   "urn:ietf:wg:oauth:2.0:oob"
 );
 
+let renewed = false;
+
 auth.on('tokens', (tokens) => {
   if (tokens.refresh_token) {
     // store the refresh_token in your secure persistent database
@@ -28,19 +27,25 @@ auth.on('tokens', (tokens) => {
       refresh_token: tokens.refresh_token,
     };
     const str = JSON.stringify(creds, true, 2);
-    //console.log(`Your 'gapi-credentials.json' has been set to: ${str}`);
+    console.log(`Gapi refresh token renewed`);
     fs.writeFileSync("./gapi-credentials.json", str);
+
+    renewed = true;
+    auth.setCredentials({
+      refresh_token: tokens.refresh_token,
+      token_type: "Bearer"
+    });
 
     //console.log(tokens.refresh_token);
   }
   //console.log(tokens.access_token);
 });
 
-auth.setCredentials({
-  access_token: "DUMMY",
-  expiry_date: 1,
-  refresh_token: creds.refresh_token,
-  token_type: "Bearer"
-});
+if (!renewed) {
+  auth.setCredentials({
+    refresh_token: creds.refresh_token,
+    token_type: "Bearer"
+  });
+}
 
 module.exports = auth;
